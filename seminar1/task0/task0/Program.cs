6e16125bcf33a6aa12930b10d6acc92e3c0c33d3
Bartoshyk Olga <eireen@mail.ru>
2023-10-01 23:23:54 +0300

    task0 task1

---
+++ seminar1/task0/task0/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("введите число");
+string number_string = Console.ReadLine()!;
+int number = Convert.ToInt32(number_string);
+int result = number * number;
+Console.WriteLine(result);
---
+++ seminar1/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите число А");
+string numberAstring = Console.ReadLine()!;
+int numberA = Convert.ToInt32(numberAstring);
+Console.WriteLine("введите число B");
+string numberBstring = Console.ReadLine()!;
+int numberB = Convert.ToInt32(numberBstring);
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
